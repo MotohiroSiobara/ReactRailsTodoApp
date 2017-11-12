36296672224831c9811b3471deb42ae9425aecb9
motohiroe shiobara <siobara.motohiro@gmail.com>
2017-11-12 10:28:10 +0900

    checkした際の処理をリファクタリング

---
+++ frontend/javascripts/todo_lists/components/AddListForm.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class AddListForm extends Component {
+  render() {
+    return (
+        <form>
+          <input type='text' />
+        </form>
+    )
+  }
+}
