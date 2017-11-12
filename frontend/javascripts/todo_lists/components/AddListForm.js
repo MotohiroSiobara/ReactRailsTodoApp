7080996519bdda425442b5d961925fd7d10810dd
motohiroe shiobara <siobara.motohiro@gmail.com>
2017-11-12 10:36:23 +0900

    todolistへの追加フォームを実装

--- frontend/javascripts/todo_lists/components/AddListForm.js
+++ frontend/javascripts/todo_lists/components/AddListForm.js
@@ -1,11 +1,29 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class AddListForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''};
+  }
+
   render() {
     return (
-        <form>
-          <input type='text' />
+      <div>
+        <h3>TODOリストに追加する</h3>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+        <input type='text' value={this.state.value} onChange={(e) => this.handleChange(e)} />
+        <input type="submit" value="追加" />
         </form>
+      </div>
     )
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 }
